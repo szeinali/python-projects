19585d1947881bfd391c7904fda4fcf35cbc1c9b
aashka-agarwal <aagarwal1@hawk.iit.edu>
2016-12-02 02:29:14 -0600

    Update summarize.py

--- a4/summarize.py
+++ a4/summarize.py
@@ -1,1 +1,24 @@
+import json
 
+with open('result1') as :    
+        users_n = json.load(data_file)
+    return users_n
+
+with open('result2') as data_file:    
+        tweet_l = json.load(data_file)
+    return tweet_l
+
+with open('ci') as data_file:    
+        csi = json.load(data_file)
+    return csi
+
+with open('classifyi') as data_file:    
+        clsi = json.load(data_file)
+    return clsi
+
+print("Number of users collected %d", len(users))
+print("Number of tweets collected %d:",len(tweet))
+print("Number of communities discovered %d:",csi[0])
+print("Average number of users per community:",csi[1])
+print("Number of instances per class found:",clsi[0][1])
+print("One example from each class:",)
