95413e787c7bd9436ef272c78e75242277c874d0
aashka-agarwal <aagarwal1@hawk.iit.edu>
2016-12-02 03:26:45 -0600

    Update summarize.py

--- a4/summarize.py
+++ a4/summarize.py
@@ -1,24 +1,31 @@
 import json
 
-with open('result1') as :    
-        users_n = json.load(data_file)
-    return users_n
+def details_download():
+        
+        with open('result1') as :    
+                users_n = json.load(data_file)
 
-with open('result2') as data_file:    
-        tweet_l = json.load(data_file)
-    return tweet_l
 
-with open('ci') as data_file:    
-        csi = json.load(data_file)
-    return csi
+        with open('result2') as data_file:    
+                tweet_l = json.load(data_file)
 
-with open('classifyi') as data_file:    
-        clsi = json.load(data_file)
-    return clsi
+        with open('ci') as data_file:    
+                csi = json.load(data_file)
+
+
+        with open('classifyi') as data_file:    
+                clsi = json.load(data_file)
 
 print("Number of users collected %d", len(users))
 print("Number of tweets collected %d:",len(tweet))
 print("Number of communities discovered %d:",csi[0])
 print("Average number of users per community:",csi[1])
 print("Number of instances per class found:",clsi[0][1])
-print("One example from each class:",)
+
+
+
+def main():
+        details_download():
+
+if __name__ == '__main__':
+        main()
